9fe249ce221a242f3961dbd889ac9bcd815d4448
Sjon Kosse <sjon@x61.eu>
2017-03-05 19:25:17 +0100

    Initial version, playing around with serial debugging to figure out mistiming issues.

--- generator.ino
+++ generator.ino
@@ -10,26 +10,38 @@ void setup()
 {
 	pinMode(in, INPUT);
 	pinMode(out, OUTPUT);
+	Serial.begin(2000000); //Baud rate of 2Mbps
+	Serial.println("Program initialized");
 }
 
 //Basic pulse train
 void loop()
 {
+	Serial.print("Start time:");
+	Serial.println(millis());
 	writeSync(out);
+	Serial.print("Finished writing the sync pulse, current time:");
+	Serial.println(millis());
 	for (int i = 0; i < trainLength; i++) {
 		write0(out);
+		Serial.print("Finished writing the ");
+		Serial.print(i);
+		Serial.print("th pulse, current time:");
+		Serial.println(millis());
 	}
+	Serial.print("Finished writing the starting train, end time:");
+	Serial.println(millis());
 }
 
 //Writes a sync signal, does not handle entry of pins not defined as output.
-void writeSync(uint8_t pin) {
+inline void writeSync(uint8_t pin) {
 	digitalWrite(pin, HIGH);
 	delay(syncTime); //15ms, TODO(sjon): change to a trap at some point?
 	digitalWrite(pin, LOW);
 }
 
 //Writes 25% low, 75% high
-void write0(uint8_t pin) {
+inline void write0(uint8_t pin) {
 	digitalWrite(pin, LOW);
 	delay(bitTime / 4);
 	digitalWrite(pin, HIGH);
@@ -37,9 +49,9 @@ void write0(uint8_t pin) {
 }
 
 //Writes 75% low, 25% high
-void write1(uint8_t pin) {
+inline void write1(uint8_t pin) {
 	digitalWrite(pin, LOW);
-	delay(bitTime - (bitTime / 4));
+	delay((bitTime/4) * 3);
 	digitalWrite(pin, HIGH);
-	delay(bitTime - (bitTime / 4));
+	delay(bitTime - (bitTime / 4) * 3);
 }
